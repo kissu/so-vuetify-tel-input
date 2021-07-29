5a83857a172837bce90244926a2f89d1ded16191
Konstantin BIFERT <konstantin.bifert@gmail.com>
2021-07-29 11:42:17 +0200

    done

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <vue-tel-input-vuetify v-model="phone"></vue-tel-input-vuetify>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      phone: ''
+    }
+  },
+}
+</script>
---
+++ plugins/phone-input.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import vuetify from "vuetify";
+import VueTelInputVuetify from 'vue-tel-input-vuetify/lib';
+
+Vue.use(VueTelInputVuetify, {
+  vuetify,
+});
